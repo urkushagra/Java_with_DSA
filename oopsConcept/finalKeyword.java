a8876d3b5e3e0e37a00baf0ee78aa115dbc9dfa5
Kushagra Upadhayay <120689974+urkushagra@users.noreply.github.com>
2025-09-20 23:00:15 +0530

    Create finalKeyword.java

---
+++ oopsConcept/finalKeyword.java
@@ -0,0 +1,10 @@
+final class Constants {
+    final double PI = 3.14159;
+}
+
+class FinalDemo {
+    public static void main(String[] args) {
+        Constants c = new Constants();
+        System.out.println("Value of PI: " + c.PI);
+    }
+}
