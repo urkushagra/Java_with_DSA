e46f7f996a8aa0d5edef87b70f3adb6170d94284
Kushagra Upadhayay <120689974+urkushagra@users.noreply.github.com>
2025-09-30 23:52:21 +0530

    Create twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] ans = twoSum(nums, 9);
+        System.out.println(ans[0] + " " + ans[1]); // 0 1
+    }
+}
