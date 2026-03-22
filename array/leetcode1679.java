f3d44ce75b0f745934e66a41632b3f288099245f
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-03-22 23:24:11 +0530

    Create leetcode1679.java

---
+++ array/leetcode1679.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        
+        for (int num : nums) {
+            int complement = k - num;
+            
+            if (map.getOrDefault(complement, 0) > 0) {
+                count++;
+                map.put(complement, map.get(complement) - 1);
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+        
+        return count;
+    }
+}
