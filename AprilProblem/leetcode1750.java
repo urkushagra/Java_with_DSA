9c481d091dd643318c18cd54f5e24a50678a1dec
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-04-11 07:32:43 +0530

    Create leetcode1750.java

---
+++ AprilProblem/leetcode1750.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char ch = s.charAt(left);
+
+            // Remove prefix
+            while (left <= right && s.charAt(left) == ch) {
+                left++;
+            }
+
+            // Remove suffix
+            while (left <= right && s.charAt(right) == ch) {
+                right--;
+            }
+        }
+
+        return right - left + 1;
+    }
+}
