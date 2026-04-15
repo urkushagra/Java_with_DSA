21ac80dc020bc8328cacf714e712337c2e02d38f
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-04-15 12:23:37 +0530

    Create leetcode1775.java

---
+++ AprilProblem/leetcode1775.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums1, int[] nums2) {
+        int sum1 = 0, sum2 = 0;
+
+        for (int x : nums1) sum1 += x;
+        for (int x : nums2) sum2 += x;
+
+        // Ensure sum1 <= sum2
+        if (sum1 > sum2) {
+            return minOperations(nums2, nums1);
+        }
+
+        int diff = sum2 - sum1;
+
+        List<Integer> gains = new ArrayList<>();
+
+        // Increase nums1
+        for (int x : nums1) {
+            gains.add(6 - x);
+        }
+
+        // Decrease nums2
+        for (int x : nums2) {
+            gains.add(x - 1);
+        }
+
+        // Sort descending
+        Collections.sort(gains, Collections.reverseOrder());
+
+        int ops = 0;
+
+        for (int g : gains) {
+            diff -= g;
+            ops++;
+
+            if (diff <= 0) return ops;
+        }
+
+        return -1;
+    }
+}
