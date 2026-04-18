c70e23645b4d11bd17ac9b312143febf38701118
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-04-18 06:36:10 +0530

    Create leetcode1785.java

---
+++ AprilProblem/leetcode1785.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        long diff = Math.abs(goal - sum);
+
+        return (int)((diff + limit - 1) / limit); // ceil division
+    }
+}
