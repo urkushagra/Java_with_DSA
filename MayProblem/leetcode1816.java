59821063a1f047f6dd12d331cdc55d195710645a
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-05-04 18:28:07 +0530

    Refactor Solution class to add truncateSentence method
    Removed maxHappyGroups method and added truncateSentence method to truncate a sentence to k words.

--- MayProblem/leetcode1816.java
+++ MayProblem/leetcode1816.java
@@ -1,56 +1,16 @@
-import java.util.*;
-
 class Solution {
-    int batchSize;
-    Map<String, Integer> memo = new HashMap<>();
+    public String truncateSentence(String s, int k) {
+        int count = 0;
 
-    public int maxHappyGroups(int batchSize, int[] groups) {
-        this.batchSize = batchSize;
-
-        int[] count = new int[batchSize];
-
-        for (int g : groups) {
-            count[g % batchSize]++;
-        }
-
-        int res = count[0]; // all divisible are happy
-
-        // pair complements
-        for (int i = 1; i <= batchSize / 2; i++) {
-            if (i == batchSize - i) {
-                res += count[i] / 2;
-                count[i] %= 2;
-            } else {
-                int min = Math.min(count[i], count[batchSize - i]);
-                res += min;
-                count[i] -= min;
-                count[batchSize - i] -= min;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    return s.substring(0, i);
+                }
             }
         }
 
-        return res + dfs(count, 0);
-    }
-
-    private int dfs(int[] count, int remain) {
-        String key = Arrays.toString(count) + "," + remain;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        int max = 0;
-
-        for (int i = 1; i < batchSize; i++) {
-            if (count[i] == 0) continue;
-
-            count[i]--;
-
-            int newRemain = (remain + i) % batchSize;
-            int happy = (remain == 0 ? 1 : 0);
-
-            max = Math.max(max, happy + dfs(count, newRemain));
-
-            count[i]++; // backtrack
-        }
-
-        memo.put(key, max);
-        return max;
+        return s;
     }
 }
