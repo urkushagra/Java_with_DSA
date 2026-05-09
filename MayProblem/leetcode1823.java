8257767e3e3ca03f688d636fbcf519c84aa9cab9
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-05-09 08:14:00 +0530

    Create leetcode1823.java

---
+++ MayProblem/leetcode1823.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int findTheWinner(int n, int k) {
+
+        int winner = 0; // 0-indexed
+
+        for (int i = 2; i <= n; i++) {
+            winner = (winner + k) % i;
+        }
+
+        return winner + 1; // convert to 1-indexed
+    }
+}
