1b894298499bbee47fd6c3d6b23b36551fc8249e
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-05-19 14:00:06 +0530

    Create leetcode1845.java

---
+++ MayProblem/leetcode1845.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class SeatManager {
+
+    private PriorityQueue<Integer> pq;
+
+    public SeatManager(int n) {
+
+        pq = new PriorityQueue<>();
+
+        for (int i = 1; i <= n; i++) {
+            pq.offer(i);
+        }
+    }
+
+    public int reserve() {
+
+        return pq.poll();
+    }
+
+    public void unreserve(int seatNumber) {
+
+        pq.offer(seatNumber);
+    }
+}
