c0f182026e535c6bfa88088ec32d04ef7d581c03
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-05-20 11:47:07 +0530

    Refactor splitString and dfs to use BigInteger

--- MayProblem/leetcode1849.java
+++ MayProblem/leetcode1849.java
@@ -1,13 +1,14 @@
+import java.math.BigInteger;
+
 class Solution {
 
     public boolean splitString(String s) {
 
         int n = s.length();
 
-        
         for (int i = 0; i < n - 1; i++) {
 
-            long first = Long.parseLong(s.substring(0, i + 1));
+            BigInteger first = new BigInteger(s.substring(0, i + 1));
 
             if (dfs(s, i + 1, first)) {
                 return true;
@@ -17,29 +18,29 @@ class Solution {
         return false;
     }
 
-    private boolean dfs(String s, int index, long prev) {
+    private boolean dfs(String s, int index, BigInteger prev) {
 
-    
         if (index == s.length()) {
             return true;
         }
 
-        long num = 0;
+        BigInteger num = BigInteger.ZERO;
 
         for (int i = index; i < s.length(); i++) {
 
-            num = num * 10 + (s.charAt(i) - '0');
+            num = num.multiply(BigInteger.TEN)
+                     .add(BigInteger.valueOf(s.charAt(i) - '0'));
 
-            // must be prev - 1
-            if (num == prev - 1) {
+            // check if num == prev - 1
+            if (num.equals(prev.subtract(BigInteger.ONE))) {
 
                 if (dfs(s, i + 1, num)) {
                     return true;
                 }
             }
 
-          
-            if (num >= prev) {
+            // pruning
+            if (num.compareTo(prev) >= 0) {
                 break;
             }
         }
