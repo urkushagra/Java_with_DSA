cf280b3de90c6df432348cfdcb71a1c141279e3b
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-06-06 14:35:33 +0530

    Create leetcode1899.java

---
+++ JuneProblem/leetcode1899.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+
+        boolean first = false;
+        boolean second = false;
+        boolean third = false;
+
+        for (int[] t : triplets) {
+
+            // Ignore invalid triplets
+            if (t[0] > target[0] ||
+                t[1] > target[1] ||
+                t[2] > target[2]) {
+                continue;
+            }
+
+            if (t[0] == target[0]) {
+                first = true;
+            }
+
+            if (t[1] == target[1]) {
+                second = true;
+            }
+
+            if (t[2] == target[2]) {
+                third = true;
+            }
+        }
+
+        return first && second && third;
+    }
+}
