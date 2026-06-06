95e5ce34c7d1cf668d7a2a986f82451eb5d8dc3b
Kushagra <120689974+urkushagra@users.noreply.github.com>
2026-06-06 14:38:38 +0530

    Update leetcode1900.java

--- JuneProblem/leetcode1900.java
+++ JuneProblem/leetcode1900.java
@@ -1,92 +1,29 @@
-import java.util.*;
-
 class Solution {
+  public int[] earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
+    int[][][][] mem = new int[n + 1][n + 1][n + 1][2];
+    return solve(firstPlayer, n - secondPlayer + 1, n, mem);
+  }
 
-    private Map<String, int[]> memo = new HashMap<>();
+  private int[] solve(int l, int r, int k, int[][][][] mem) {
+    if (l == r)
+      return new int[] {1, 1};
+    if (l > r)
+      return solve(r, l, k, mem);
+    if (!Arrays.equals(mem[l][r][k], new int[] {0, 0}))
+      return mem[l][r][k];
 
-    public int[] earliestAndLatest(int n,
-                                   int firstPlayer,
-                                   int secondPlayer) {
+    int a = Integer.MAX_VALUE;
+    int b = Integer.MIN_VALUE;
+      
+    for (int i = 1; i <= l; ++i)
+      for (int j = l - i + 1; j <= r - i; ++j) {
+        if (i + j > (k + 1) / 2 || i + j < l + r - k / 2)
+          continue;
+        int[] res = solve(i, j, (k + 1) / 2, mem);
+        a = Math.min(a, res[0] + 1);
+        b = Math.max(b, res[1] + 1);
+      }
 
-        return dfs(n, firstPlayer, secondPlayer);
-    }
-
-    private int[] dfs(int n, int a, int b) {
-
-        if (a > b) {
-            int t = a;
-            a = b;
-            b = t;
-        }
-
-        if (a + b == n + 1) {
-            return new int[]{1, 1};
-        }
-
-        String key = n + "," + a + "," + b;
-
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        int earliest = Integer.MAX_VALUE;
-        int latest = Integer.MIN_VALUE;
-
-        int nextN = (n + 1) / 2;
-
-        for (int na = 1; na <= nextN; na++) {
-            for (int nb = na + 1; nb <= nextN; nb++) {
-
-                if (!reachable(n, a, b, na, nb)) {
-                    continue;
-                }
-
-                int[] res = dfs(nextN, na, nb);
-
-                earliest = Math.min(earliest, res[0] + 1);
-                latest = Math.max(latest, res[1] + 1);
-            }
-        }
-
-        int[] ans = new int[]{earliest, latest};
-        memo.put(key, ans);
-        return ans;
-    }
-
-    private boolean reachable(int n,
-                              int a,
-                              int b,
-                              int na,
-                              int nb) {
-
-        int leftA = a - 1;
-        int middle = b - a - 1;
-        int right = n - b;
-
-        int countA = 0;
-        int countB = 0;
-
-        for (int x = 0; x <= leftA; x++) {
-
-            int y = na - 1 - x;
-
-            if (y < 0 || y > middle) {
-                continue;
-            }
-
-            int remainMiddle = middle - y;
-
-            for (int z = 0; z <= remainMiddle; z++) {
-
-                int posB =
-                        x + y + 1 + z + 1;
-
-                if (posB == nb) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
+    return mem[l][r][k] = new int[] {a, b};
+  }
 }
